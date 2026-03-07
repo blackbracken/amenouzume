f891ff5efc153515553507f2b6b7615e8f7c20f0
blackbracken <me@bracken.black>
2026-03-07 13:30:57 +0900

    chore: update libs to build ui

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
   alias(libs.plugins.composeMultiplatform)
   alias(libs.plugins.composeCompiler)
   alias(libs.plugins.composeHotReload)
+  alias(libs.plugins.kotlinSerialization)
   alias(libs.plugins.ktlint)
   alias(libs.plugins.sqldelight)
 }
@@ -35,6 +36,8 @@ kotlin {
       implementation(compose.components.uiToolingPreview)
       implementation(libs.androidx.lifecycle.viewmodelCompose)
       implementation(libs.androidx.lifecycle.runtimeCompose)
+      implementation(libs.kotlinx.serialization.core)
+      implementation(libs.navigation3.runtime)
       implementation(libs.sqldelight.coroutines)
     }
     commonTest.dependencies {
