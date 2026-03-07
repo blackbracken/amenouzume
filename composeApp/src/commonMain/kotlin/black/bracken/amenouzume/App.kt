5a962530f5f68131f914a290c73b538598d987bb
blackbracken <me@bracken.black>
2026-03-08 01:10:44 +0900

    feat: create AmenouzumeTheme

--- composeApp/src/commonMain/kotlin/black/bracken/amenouzume/App.kt
+++ composeApp/src/commonMain/kotlin/black/bracken/amenouzume/App.kt
@@ -1,17 +1,17 @@
 package black.bracken.amenouzume
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import black.bracken.amenouzume.uishared.navigation.AppNavHost
 import black.bracken.amenouzume.uishared.navigation.OpenDatabaseRoute
+import black.bracken.amenouzume.uishared.theme.AmenouzumeTheme
 import org.koin.compose.KoinContext
 
 @Composable
 fun App() {
   KoinContext {
-    MaterialTheme {
+    AmenouzumeTheme {
       val backStack = remember { mutableStateListOf<Any>(OpenDatabaseRoute) }
       AppNavHost(backStack = backStack)
     }
--- composeApp/src/commonMain/kotlin/black/bracken/amenouzume/uishared/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/black/bracken/amenouzume/uishared/navigation/AppNavHost.kt
@@ -1,11 +1,11 @@
 package black.bracken.amenouzume.uishared.navigation
 
 import androidx.compose.runtime.Composable
-import black.bracken.amenouzume.feature.opendatabase.OpenDatabaseScreen
+import black.bracken.amenouzume.feature.opendatabase.OpenDatabaseCoordinator
 
 @Composable
 fun AppNavHost(backStack: List<Any>) {
     when (backStack.lastOrNull()) {
-        is OpenDatabaseRoute -> OpenDatabaseScreen()
+        is OpenDatabaseRoute -> OpenDatabaseCoordinator()
     }
 }
