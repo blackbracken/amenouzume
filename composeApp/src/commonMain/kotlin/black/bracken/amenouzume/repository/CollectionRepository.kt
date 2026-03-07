1a6ea1f241ad574f4ae0becc3b0605199ffc40af
blackbracken <me@bracken.black>
2026-03-07 13:26:38 +0900

    fix: remove unneeded code

--- composeApp/src/commonMain/kotlin/black/bracken/amenouzume/repository/CollectionRepository.kt
+++ composeApp/src/commonMain/kotlin/black/bracken/amenouzume/repository/CollectionRepository.kt
@@ -2,7 +2,6 @@ package black.bracken.amenouzume.repository
 
 import black.bracken.amenouzume.db.AppDatabase
 import black.bracken.amenouzume.db.Collection
-import kotlin.random.Random
 
 class CollectionRepository(
   private val database: AppDatabase,
@@ -13,22 +12,7 @@ class CollectionRepository(
 
   fun getCollectionById(id: String): Collection? = queries.selectById(id).executeAsOneOrNull()
 
-  fun insertCollection(
-    id: String,
-    path: String,
-  ) {
-    queries.insert(id, path)
-  }
-
-  fun insertRandomCollection() {
-    val randomId = generateRandomId()
-    val randomPath = "/path/to/collection/$randomId"
-    insertCollection(randomId, randomPath)
-  }
-
   fun deleteCollection(id: String) {
     queries.deleteById(id)
   }
-
-  private fun generateRandomId(): String = Random.nextInt(100000, 999999).toString()
 }
