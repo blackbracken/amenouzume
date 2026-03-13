d22427bec7e36a77f5037679a845e6d5c60e7003
blackbracken <me@bracken.black>
2026-03-13 20:55:27 +0900

    refactor: remove unused methods from CollectionRepository

--- composeApp/src/commonMain/kotlin/black/bracken/amenouzume/kernel/repository/CollectionRepository.kt
+++ composeApp/src/commonMain/kotlin/black/bracken/amenouzume/kernel/repository/CollectionRepository.kt
@@ -1,7 +1,6 @@
 package black.bracken.amenouzume.kernel.repository
 
 import black.bracken.amenouzume.db.AppDatabase
-import black.bracken.amenouzume.db.Collection
 import dev.zacsweers.metro.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -11,14 +10,6 @@ class CollectionRepository(
   private val database: AppDatabase,
 ) {
   private val queries = database.collectionQueries
-
-  fun getAllCollections(): List<Collection> = queries.selectAll().executeAsList()
-
-  fun getCollectionById(id: String): Collection? = queries.selectById(id).executeAsOneOrNull()
-
-  fun deleteCollection(id: String) {
-    queries.deleteById(id)
-  }
 
   suspend fun addCollection(
     id: String,
